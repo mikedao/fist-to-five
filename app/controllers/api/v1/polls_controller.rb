1902af4ee0ce00997f718e592c21b537f2121dbc
Robert Gu <robertgu84@gmail.com>
2021-09-22 13:54:14 -0600

    Slackbot integration

--- app/controllers/api/v1/polls_controller.rb
+++ app/controllers/api/v1/polls_controller.rb
@@ -2,7 +2,13 @@ class Api::V1::PollsController < ApplicationController
   skip_forgery_protection
 
   def create
-    poll = Poll.create(question: params[:question])
-    render json: poll
+    poll = Poll.create(question: params[:text])
+    response =
+        {
+          "response_type": "in_channel",
+          "text": "New Fist of Five: #{poll.question} \n Url: #{request.base_url}/polls/#{poll.id}"
+        }
+
+    render json: response, status: :ok
   end
 end
